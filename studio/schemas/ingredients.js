0b88ff502ea52174a566afaa464e2cca5d522528
vignesh-v97 <vigneshbharathi34@gmail.com>
2021-11-27 22:15:28 +0530

    sanity init

---
+++ studio/schemas/ingredients.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+ name: "ingredient",
+ title: "Ingredient",
+ type: "document",
+ fields: [
+  {
+   name: "name",
+   title: "Ingredient Name",
+   type: "string"
+  },
+  {
+   name: "image",
+   title: "Image",
+   type: "image",
+   option: {
+    hotspot: true,
+   }
+  },
+  {
+   name: "notes",
+   title: "Notes",
+   type: "text"
+  }
+ ]
+}
